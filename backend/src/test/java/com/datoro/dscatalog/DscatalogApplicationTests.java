3fdfc0f7af71f708fe1298a254cb88436c4694f9
Mario Datoro <mario.datoro@gmail.com>
2020-09-28 21:36:07 -0300

    Project created

---
+++ backend/src/test/java/com/datoro/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datoro.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
