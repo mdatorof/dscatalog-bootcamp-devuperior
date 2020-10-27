83d77d425a6fba74a736916db1872169dfdfd7f4
Mario Datoro <mario.datoro@gmail.com>
2020-10-27 11:06:44 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/datoro/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.datoro.dscatalog.dto;
+
+import com.datoro.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+}
--- backend/src/main/java/com/datoro/dscatalog/services/UserService.java
+++ backend/src/main/java/com/datoro/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.datoro.dscatalog.dto.RoleDTO;
 import com.datoro.dscatalog.dto.UserDTO;
 import com.datoro.dscatalog.dto.UserInsertDTO;
+import com.datoro.dscatalog.dto.UserUpdateDTO;
 import com.datoro.dscatalog.entities.Role;
 import com.datoro.dscatalog.entities.User;
 import com.datoro.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToEntity(dto, entity);
